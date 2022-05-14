538d6af02355393068366cad2620cb7e57ff660d
Wang Lyle <lyle.wang@outlook.com>
2022-05-14 18:07:53 +0800

    update

--- resources/gunicorn_conf.py
+++ resources/gunicorn_conf.py
@@ -6,7 +6,7 @@ workers_per_core_str = os.getenv("WORKERS_PER_CORE", "2")
 web_concurrency_str = os.getenv("WEB_CONCURRENCY", None)
 
 host = os.getenv("HOST", "0.0.0.0")
-port = os.getenv("PORT", "8000")
+port = "8000"
 bind_env = os.getenv("BIND", None)
 use_loglevel = os.getenv("LOG_LEVEL", "info")
 if bind_env:
