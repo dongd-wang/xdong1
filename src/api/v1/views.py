b76f4b961dc2d5aecb955ef29e34df569d00300d
lwang233 <105355675+lwang233@users.noreply.github.com>
2022-05-20 10:07:02 +0800

    Update views.py

--- src/api/v1/views.py
+++ src/api/v1/views.py
@@ -40,7 +40,7 @@ async def generate():
 async def get_v2_url():
     try:
         async with httpx.AsyncClient(headers=get_headers()) as client:
-            resp = await client.get(os.environ.get('V2_URL'))
+            resp = await client.get(os.getenv('V2_URL'))
             if resp.status_code >=400:
                 return 'hello v2 error'
             v2_sub_url_list = re.findall('https://shadowshare.v2cross.com/publicserver/servers/temp/.{10,20}<', resp.content.decode('UTF-8'))
